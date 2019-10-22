1f9177c8866f426b3657c8b79d668b2a4132e01f
JRathbone <justinrathbone@gmail.com>
2019-10-21 21:21:25 -0500

    adding project files to github for the API

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'Justin',
+        password: '',
+        database: 'smart-brain'
+    }
+})
+
+const cors = require('cors');
+const app = express();
+app.use(cors())
+app.use(bodyParser.json());
+app.get('/',(req,res) =>{
+    res.json(db.users);
+})
+
+app.post('/signin', (req,res) => {signin.handleSignin(req,res,db,bcrypt)});
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)});
+app.get('/profile/:id', (req,res) => {profile.handleProfile(req,res,db)});
+app.put('/image', (req,res) => {image.handleImage(req,res,db)});
+app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)})
+app.listen(3000,() => {
+
+});
